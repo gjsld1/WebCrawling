13dd3ad0b5a4d7920ce3df4e8a51900314a0caf2
신예지 <gjsld1@naver.com>
2019-08-07 22:40:14 +0900

    19/08/07 crawling 8seconds necessaryInfo

--- crawl8seconds.py
+++ crawl8seconds.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 import time
 
 driver = webdriver.Chrome(
-    executable_path="../webdriver/chromedriver"
-)
+                          executable_path="../webdriver/chromedriver"
+                          )
 
 url="http://www.ssfshop.com/8seconds/GM0018120501196/good?dspCtgryNo=SFMA41A02&brandShopNo=BDMA07A01&brndShopId=8SBSS&keyword=&leftBrandNM=&utag=ref_cat:SFMA41A02$ref_brn:BDMA07A01$ref_gtp:GNRL_CTGRY$ref_br:8SBSS$set:1$dpos:1"
 driver.get(url)
@@ -17,9 +17,17 @@ html = driver.page_source
 # HTML 소스코드를 파이썬 객체로 변환
 soup = BeautifulSoup(html, 'html.parser')
 
-results = soup.select("#goodsMySizeList > tbody > tr")
-for result in results:
-    print(result.text)
+additionalInfos = soup.select("div.wear > div > dl")
+for additionalInfo in additionalInfos:
+    print(additionalInfo.text)
+
+clothSizes = soup.select("table.tbl_info[summary='Size'] > tbody > tr")
+for clothSize in clothSizes:
+    print(clothSize.text)
+
+mySizeLists = soup.select("#goodsMySizeList > tbody > tr")
+for mySizeList in mySizeLists:
+    print(mySizeList.text)
 
 
 #results = soup.find_all('table', id='goodsMySizeList')
