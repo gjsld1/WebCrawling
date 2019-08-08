b1f1602a73859c6f24271582762c2375263735f1
신예지 <gjsld1@naver.com>
2019-08-09 00:02:43 +0900

    19/08/27 crawl 8seconds reviews

--- crawl8seconds.py
+++ crawl8seconds.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from itertools import count
 import time
+import pandas as pd
 
 driver = webdriver.Chrome(
                           executable_path="../webdriver/chromedriver"
@@ -9,7 +11,6 @@ driver = webdriver.Chrome(
 
 url="http://www.ssfshop.com/8seconds/GM0018120501196/good?dspCtgryNo=SFMA41A02&brandShopNo=BDMA07A01&brndShopId=8SBSS&keyword=&leftBrandNM=&utag=ref_cat:SFMA41A02$ref_brn:BDMA07A01$ref_gtp:GNRL_CTGRY$ref_br:8SBSS$set:1$dpos:1"
 driver.get(url)
-
 time.sleep(5)
 
 html = driver.page_source
@@ -25,9 +26,30 @@ clothSizes = soup.select("table.tbl_info[summary='Size'] > tbody > tr")
 for clothSize in clothSizes:
     print(clothSize.text)
 
-mySizeLists = soup.select("#goodsMySizeList > tbody > tr")
-for mySizeList in mySizeLists:
-    print(mySizeList.text)
+
+reviewCnt = soup.find("em", id="review_size_h3_em").text
+pageNum = int(reviewCnt)/10
+temp = int(pageNum)
+if(pageNum-temp!=0): pageNum=temp+1
+
+results = []
+for page in count(1):
+    script = 'getMySizeSet(0,%d,2)'%page #js코드
+    driver.execute_script(script) #js실행
+    time.sleep(5)
+    
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    mySizeLists = soup.select("#goodsMySizeList > tbody > tr")
+    for mySizeList in mySizeLists:
+        print(mySizeList.text, page)
+    
+    next = soup.select("#mySizePaging")
+    if next is None:
+        break
+
+
 
 
 #results = soup.find_all('table', id='goodsMySizeList')
